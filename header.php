b4d8de311359339671fdcfc59d6e42acfc9df2c0
Noël Jörg <noelgamemc@gmail.com>
2024-02-06 15:30:39 +0100

    change header - HTML only

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <header>
     <nav>
         <ul>
-            <li><a href="user.php">Dashboard</a></li>
-        </ul>
+        <li><a href="user.php">Dashboard</a></li>
+        <li><a href="https://benedict.ch">Bénédict.ch</a></li>
+        <li><a href="https://github.com/marcel7ag/m295projekt">Source Code</a></li>
+    </ul>
+
 </header>
